fa2534938d6a1bea301571597b71f9300193baa5
Joe King <j.king92@gmail.com>
2022-09-14 08:17:43 -0700

    Add methods to each variable

--- arrays.rb
+++ arrays.rb
@@ -10,3 +10,19 @@ prices = [2.50, 14.75, 7.77, 8.99]
 
 #array of booleans
 likes_coffee = [true, false, true, true]
+
+#Calls the unshift method on the family_members Array
+#This will add "joe" as first element of the array
+family_members.unshift("joe")
+
+#This line calls the pop method on the ages array
+#it will remove the last element
+ages.pop
+
+#This calls the shift method on the likes_coffee array
+#it will remove the first element
+likes_coffee.shift
+
+#This calls the push method on the prices Array
+#It will add the listed element(s) to the end of the Array
+prices.push(1.23, 4.56, 7.89)
