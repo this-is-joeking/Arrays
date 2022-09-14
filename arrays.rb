aefe33af3610c217e65d9ef4adeb1e8f8e238cd7
Joe King <j.king92@gmail.com>
2022-09-14 08:08:50 -0700

    Add variables

---
+++ arrays.rb
@@ -0,0 +1,12 @@
+#Arrays Practive
+#Array of strings
+family_members = ["Jamie", "Ash", "Jerry", "Lori"]
+
+#Array of integers
+ages = [32, 34, 65, 64]
+
+#array of floats
+prices = [2.50, 14.75, 7.77, 8.99]
+
+#array of booleans
+likes_coffee = [true, false, true, true]
